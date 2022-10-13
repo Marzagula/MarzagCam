55d1098f7904b1f50db018ce432f8b478617f8e0
Marzag <korgoon@gmail.com>
2022-10-13 20:39:43 +0200

    CAM-11 owner_id should not be unique

--- pgscripts/tables/camera.sql
+++ pgscripts/tables/camera.sql
@@ -1,6 +1,6 @@
 create table CAMERA (
 id serial primary key,
-owner_id varchar(50) unique not null,
+owner_id varchar(50) not null,
 camera_id varchar(50) not null,
 application_path varchar(1000) not null,
 application_version varchar(100) not null
