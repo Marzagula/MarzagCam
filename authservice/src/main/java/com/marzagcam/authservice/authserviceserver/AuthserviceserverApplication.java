3ec44e88bf6f8d68752ac2eaa46aa8b17e9f329e
Mateusz Parzys <mateuszparzysoffice@gmail.com>
2022-10-21 17:59:34 +0200

    init commit

---
+++ authservice/src/main/java/com/marzagcam/authservice/authserviceserver/AuthserviceserverApplication.java
@@ -0,0 +1,13 @@
+package com.marzagcam.authservice.authserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserviceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserviceserverApplication.class, args);
+	}
+
+}
---
+++ authservice/src/test/java/com/marzagcam/authservice/authserviceserver/AuthserviceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marzagcam.authservice.authserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
